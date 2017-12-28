1888449a0798f29df4d9b0c43f100481939793f1
Gustavo <gusta.p.o@hotmail.com>
2017-12-28 19:40:36 +0000

    Enviar email depois de registar User 80%Feito (Bug)

--- laravel_server/resources/assets/js/components/Register.vue
+++ laravel_server/resources/assets/js/components/Register.vue
@@ -10,22 +10,22 @@
                     <div class="panel-body">
 
                         <!--Nome-->
-                        <div class="form-group">
+                        <div class="form-group" :class="{'has-error': errors.name.lenght}">
                             <input v-model="User.name" class="form-control" type="text" placeholder="Nome completo"/>
                         </div>
 
                         <!--Nickname-->
-                        <div class="form-group">
+                        <div class="form-group" :class="{'has-error': errors.nickname.lenght}">
                             <input v-model="User.nickname" class="form-control" type="text" placeholder="Nickname"/>
                         </div>
 
                         <!--Email-->
-                        <div class="form-group">
+                        <div class="form-group" :class="{'has-error': errors.email.lenght}">
                             <input v-model="User.email" class="form-control" type="email" placeholder="Email"/>
                         </div>
 
                         <!--Password-->
-                        <div class="form-group">
+                        <div class="form-group" :class="{'has-error': errors.password.lenght}">
                             <input v-model="User.password" class="form-control" type="password" placeholder="Password"/>
                         </div>
 
@@ -33,7 +33,6 @@
                         <button @click.prevent="registerUser" class="btn btn-success pull-right">
                             Registar
                         </button>
-
 
                     </div>
 
@@ -53,6 +52,12 @@
                     nickname: '',
                     email: '',
                     password: ''
+                },
+                errors: {
+                    name: [],
+                    nickname: [],
+                    email: [],
+                    password: []
                 }
 
             }
@@ -60,8 +65,18 @@
 
 
         methods: {
-            registerUser() {
 
+            resetUser() {
+                this.user =
+                    {
+                        name: null,
+                        nickname: null,
+                        email: null,
+                        password: null
+                    }
+            },
+
+            registerUser() {
                 let newUser =
                     {
                         name: this.User.name,
@@ -71,17 +86,39 @@
                     }
 
                 console.log(newUser);
+
                 axios.post('/api/register', newUser)
                     .then((response) => {
-                        console.log(response);
+                        //console.log(response);
+                        this.resetUser()
+                        let successMessage= response.data.message;
+                        alert('Sucess'+sucessMessage);
                     })
                     .catch((error) => {
-                    let data= error.response.data;
 
+
+
+                    //Show errors
+                        let data = error.response.data;
                         console.log(data);
+                        for (let key in this.errors) {
+                            this.errors[key] = []
+                            let errorMessage = data[key];
+                            console.log('key  ', key);
+                           console.log('errorMessage: ', response.data.message);
+                        alert('Error' + errorMessage);
+                            if (errorMessage) {
+                                this.error[key] = errorMessage;
+                            }
+                        }
+
+
                     });
 
-console.log("UTILIZADOR CRIADO COM SUCESSO");
+                console.log("UTILIZADOR CRIADO COM SUCESSO");
+
+                this.$router.push("/users");
+
                 //TODO: ONClick register -> enviar email de confirmação
             }
         }
