217c16e195c9b0e05a4228bab890178ca5e8f3a0
pffigueiredo <klisarkk@gmail.com>
2018-01-03 01:45:28 +0000

    A tentar começar cenas no multiplayer

--- laravel_server/resources/assets/js/components/multiplayer_game.vue
+++ laravel_server/resources/assets/js/components/multiplayer_game.vue
@@ -1,23 +1,106 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        Component  multiplayer -> OK
-                    </div>
-                </div>
-            </div>
+    <div >
+        <div >
+            <h3 class="text-center">{{ title }}</h3>
+            <br>
+            <h2>Current Player : {{ currentPlayer }}</h2>
+            <p>Set current player name <input v-model.trim="currentPlayer"></p>
+            <p><em>Player name replaces authentication! Use different names on different browsers, and don't change it frequently.</em></p>
+            <hr>
+            <h3 class="text-center">Lobby</h3>
+            <p><button class="btn btn-xs btn-success" v-on:click.prevent="createGame">Create a New Game</button></p>
+            <hr>
+            <lobby :games="lobbyGames"></lobby>
+            <h4>Pending games (<a @click.prevent="loadLobby">Refresh</a>)</h4>
         </div>
     </div>
 </template>
 
-<script>
-    export default {
-        mounted() {
-            console.log('Component multiplayer mounted.')
+<script type="text/javascript">
+
+import Lobby from './lobby.vue';
+
+export default {
+    data: function(){
+        return {
+            title: 'Multiplayer TicTacToe',
+            currentPlayer: 'Player X',
+            lobbyGames: [],
+            activeGames: [],
+            socketId: "",
         }
-    }
+    },
+    sockets : {
+        connect(){
+            console.log('socket connected');
+            this.socketId = this.$socket.id;
+        },
+        discconnect(){
+            console.log('socket disconnected');
+            this.socketId = "";
+        },
+        lobby_changed(){
+            this.loadLobby();
+        },
+        my_active_games_changed(){
+            this.loadActiveGames();
+        },
+        my_active_games(games){
+            this.activeGames = games;
+        },
+        my_lobby_games(games){
+            this.lobbyGames = games;
+        },
+        game_changed(game){
+            for (var lobbyGame of this.lobbyGames) {
+                if (game.gameID == lobbyGame.gameID) {
+                    Object.assign(lobbyGame, game);
+                    break;
+                }
+            }
+            for (var activeGame of this. activeGames) {
+                if (game.gameID == activeGame.gameID) {
+                    Object.assign(activeGame, game);
+                    break;
+                }
+            }
+        },
+
+    },
+    methods: {
+      createGame(){
+        if (this.currentPlayer == "") {
+            alert('Current Player is Empty - Cannot Create a Game');
+            return;
+        }
+        else {
+            this.$socket.emit('create_game', { playerName: this.currentPlayer });   
+        }
+    },
+    loadLobby(){
+        this.$socket.emit('get_my_lobby_games');
+    },
+    loadActiveGames(){
+        this.$socket.emit('get_my_activegames');
+    },
+    createGame(){
+        if (this.currentPlayer == "") {
+            alert('Current Player is Empty - Cannot Create a Game');
+            return;
+        }
+        else {
+            this.$socket.emit('create_game', { playerName: this.currentPlayer });   
+        }
+    },
+
+},
+
+components: {
+    'lobby': Lobby,
+},
+
+mounted() {
+    console.log('Component multiplayer mounted.')
+}
+}
 </script>
